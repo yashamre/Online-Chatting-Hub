7e38e83c782c3f6336d4190e18c551146875a7f5
kunalAkhade <bialk@LAPTOP-C3TGDGH0>
2023-03-18 00:54:16 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express=require('express')
+const app=express();
+const  socketio  = require('socket.io');
+const path=require('path');
+const http=require('http');
+const { Socket } = require('dgram');
+const server=http.createServer(app);
+const io= socketio(server);
+app.use(express.static(path.join(__dirname,'public')));
+
+const users=[];
+
+io.on('connection',socket =>{
+  //  console.log("connected")
+    socket.on("joinRoom", ({ userName, userPassword }) => {
+         // console.log({userName,userPassword});
+          socket.join(userPassword)
+          const id=socket.id;
+          users.push({id , userName, userPassword})
+          console.log(users)
+          socket.emit("users", users)
+          socket.broadcast.to(userPassword).emit("user", userName)
+
+      });
+
+    
+
+    socket.on('disconnect',()=>{
+        const id=socket.id;
+        const index = users.findIndex(user => user.id === id);
+        users.splice(index, 1)
+        console.log(users)
+        socket.broadcast.emit("leave",users)
+    })  
+
+    socket.on('chat-message',message=>{
+
+        const currentUser=users.find(function(element){
+            return element.id==socket.id;
+        });
+        const username=currentUser.userName;
+        console.log(currentUser.userName)
+        
+        io.to(currentUser.userPassword).emit('message',{username,message})
+    })
+     
+    
+})
+
+
+server.listen(3000 ,function(){
+   console.log("server running...")
+});
+
---
+++ public/js/index.js
@@ -0,0 +1,77 @@
+const { userName, userPassword } = Qs.parse(window.location.search, {
+    ignoreQueryPrefix: true,
+  });
+  
+  
+console.log(userName,userPassword)
+const socket=io();
+
+
+socket.emit("joinRoom", { userName, userPassword });
+
+socket.on("users",(users)=>{
+   
+     users.forEach(user => {
+      if(socket.id!=user.id){
+          if(user.userPassword==userPassword){
+           const ul=document.querySelector("ul");
+           const li=document.createElement("li");
+           li.innerHTML="<h2>"+user.userName+"</h2>";
+           li.classList.add("listItem")
+           ul.appendChild(li);
+          }
+        }
+     });
+    
+})
+
+socket.on("user",(userName) => {
+  const ul=document.querySelector("ul");
+  const li=document.createElement("li");
+  li.innerHTML="<h2>"+userName+"</h2>";
+  li.classList.add("listItem")
+  ul.appendChild(li);
+})
+
+socket.on('message',({username, message})=>{
+    const chatBox=document.querySelector(".chatBox");
+const messageDiv=document.createElement("div");
+messageDiv.innerHTML="<span>"+username+"</span>"+"<br>"+message;
+chatBox.appendChild(messageDiv)
+messageDiv.classList.add('message');
+
+chatBox.scrollTop=chatBox.scrollHeight
+  
+})
+
+socket.on("leave",(users)=>{
+  const ul=document.querySelector("ul");
+  ul.innerHTML=" ";
+  users.forEach(user => {
+    if(socket.id!=user.id){
+    const ul=document.querySelector("ul");
+   const li=document.createElement("li");
+   li.innerHTML="<h2>"+user.userName+"</h2>";
+   li.classList.add("listItem")
+   ul.appendChild(li);
+   }
+   });
+
+})
+
+const Button=document.querySelector(".Send")
+
+
+Button.addEventListener('click', (event) => {
+    event.preventDefault();
+   const msg= document.querySelector(".TextBox").value;
+   socket.emit('chat-message',msg);
+   document.querySelector(".TextBox").value="";
+   
+});
+
+
+
+
+
+
